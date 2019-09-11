bdf947a4e6079a2eed9cd32485a1de7efb00e2cf
Bryan Atsatt <bryan.atsatt@oracle.com>
2019-09-11 15:51:32 -0700

    Configure jlink with dummy module to avoid automatic module error.

--- jlink/plugin/src/main/java9/io/helidon/jlink/plugins/HelidonPlugin.java
+++ jlink/plugin/src/main/java9/io/helidon/jlink/plugins/HelidonPlugin.java
@@ -16,9 +16,12 @@
 
 package io.helidon.jlink.plugins;
 
-import java.util.Arrays;
-import java.util.List;
+import java.lang.module.ModuleFinder;
+import java.lang.module.ModuleReference;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
+import java.util.Set;
 
 import jdk.tools.jlink.plugin.Plugin;
 import jdk.tools.jlink.plugin.ResourcePool;
@@ -28,10 +31,10 @@ import jdk.tools.jlink.plugin.ResourcePoolBuilder;
  * TODO: Describe
  */
 public class HelidonPlugin implements Plugin {
-
     private static final String NAME = "helidon";
-    private String targetModule;
-    private List<String> modules;
+
+    private Path appModulePath;
+    private Path appLibsDir;
 
     @Override
     public String getName() {
@@ -45,13 +48,26 @@ public class HelidonPlugin implements Plugin {
 
     @Override
     public void configure(Map<String, String> config) {
-        targetModule = config.get( NAME );
-        String modulesToIndex = config.get( "for-modules" );
-        this.modules = Arrays.asList(modulesToIndex.split("," ) );
+        appModulePath = Paths.get(config.get(NAME));
+        appLibsDir = appModulePath.getParent().resolve("libs");
+        System.out.println("appModulePath: " + appModulePath);
+        System.out.println("   appLibsDir: " + appModulePath);
     }
 
     @Override
     public ResourcePool transform(ResourcePool in, ResourcePoolBuilder out) {
         return null;
     }
+
+
+    private static String getModuleName(Path modulePath) {
+        ModuleFinder finder = ModuleFinder.of(modulePath);
+        Set<ModuleReference> modules = finder.findAll();
+        if (modules.size() == 1) {
+            return modules.iterator().next().descriptor().name();
+        } else {
+            throw new IllegalArgumentException(modulePath + " does not point to a module");
+        }
+    }
+
 }
